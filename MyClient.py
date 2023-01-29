db0586977023411b2525d250dd0eb8e78dde1db3
wintee <b12e34e56@naver.com>
2023-01-29 14:30:58 +0900

    검색 기능 추가 / 아이템 명령어 개편

--- MyClient.py
+++ MyClient.py
@@ -1,6 +1,5 @@
 import asyncio
-import time
-
+from datetime import datetime
 import discord
 from discord.ext import commands
 import EFTChangesAPI
@@ -14,7 +13,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(bot.user.name, "로 로그인 함")
+    print(datetime.today().strftime("%Y-%m-%d %H:%M:%S"), bot.user.name, "로 로그인 함")
 
 
 @bot.event
@@ -25,7 +24,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def 탄약(ctx, *, message):
-    print("[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "탄약 명령어 호출함")
+    print(datetime.today().strftime("%Y-%m-%d %H:%M:%S"), "[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "탄약 명령어 호출함")
     search_result = EFTChangesAPI.ammo_search(message)
     await ctx.send(search_result)
 
@@ -38,7 +37,7 @@ async def 탄약_error(ctx, error):
 
 @bot.command()
 async def 지도(ctx, *, message):
-    print("[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "지도 명령어 호출함")
+    print(datetime.today().strftime("%Y-%m-%d %H:%M:%S"),"[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "지도 명령어 호출함")
     if message == "공장":
         await ctx.send(file=discord.File("data/공장_지도.png"))
     elif message == "나들목":
@@ -70,26 +69,68 @@ async def 지도_error(ctx, error):
 
 
 @bot.command()
-async def 아이템(ctx, *, message):
-    print("[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "아이템 명령어 호출함")
+async def 가격(ctx, *, message):
+    print(datetime.today().strftime("%Y-%m-%d %H:%M:%S"),"[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "가격 명령어 호출함")
+    loop = asyncio.get_event_loop()
     path = "resource/chromedriver.exe"
     driver = webdriver.Chrome(path)
 
-    driver.get("https://tarkov-market.com/")
+    await loop.run_in_executor(None, driver.get, "https://tarkov-market.com/")
     element = driver.find_element_by_css_selector("""#__layout > div > div > div.page-content > div.w-100 > div.search > input[type=text]""")
-    element.send_keys(message)
+    await loop.run_in_executor(None, element.send_keys, message)
 
-    await asyncio.sleep(2)
+    await asyncio.sleep(1.5)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    price = soup.select_one("""#__layout > div > div > div.page-content > div.w-100 > div.cards-list > div > div.body > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)""").text
-    wiki = soup.select_one("""#__layout > div > div > div.page-content > div.w-100 > div.cards-list > div > div.image > div.block.text-center > a""")["href"]
 
-    await ctx.send("가격 : " + price + "\n 위키 링크 : " + wiki)
-    driver.close()
+    if len(soup.find(class_="cards-list").text) > 100:
+        if soup.find(class_="minus"):
+            await ctx.send("플리마켓에 올릴 수 없는 상품입니다.")
+
+        else:
+            price = soup.select_one("#__layout > div > div > div.page-content > div.w-100 > div.cards-list > div > div.body > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)").text
+            await ctx.send(message + " : " + price)
+
+    else:
+        await ctx.send("[오류] 아이템이 존재하지 않습니다.")
+
+    await loop.run_in_executor(None, driver.close)
 
 
-@아이템.error
-async def 아이템_error(ctx, error):
+@가격.error
+async def 가격_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("[오류] 잘못된 명령어. 검색어를 입력해주세요")
+
+
+@bot.command()
+async def 검색(ctx, *, message):
+    print(datetime.today().strftime("%Y-%m-%d %H:%M:%S"),"[", ctx.message.guild.name, "]", "[", ctx.message.channel, "] [", ctx.message.author, "] ", "검색 명령어 호출함")
+    loop = asyncio.get_event_loop()
+    path = "resource/chromedriver.exe"
+    driver = webdriver.Chrome(path)
+
+    await loop.run_in_executor(None, driver.get, "https://escapefromtarkov.fandom.com/wiki/Escape_from_Tarkov_Wiki")
+    search_button = driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[1]/header/div/div[3]/a[1]")
+    search_button.click()
+
+    await asyncio.sleep(1)
+    search_bar = driver.find_element_by_xpath("/html/body/div[10]/div/form/input")
+    search_bar.send_keys(message)
+
+    await asyncio.sleep(1)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    if soup.find(class_="wds-list wds-is-linked SearchResults-module_results__k8itn") is not None:
+        link = soup.select_one("body > div.search-modal > div > ul > li:nth-child(1) > a")["href"]
+        await ctx.send(link)
+    else:
+        await ctx.send("[오류] 항목이 존재하지 않습니다.")
+
+    await loop.run_in_executor(None, driver.close)
+
+
+@검색.error
+async def 검색_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("[오류] 잘못된 명령어. 검색어를 입력해주세요")
 
